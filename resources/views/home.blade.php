2b65d2ffd36312ffabd47c0bdbb761170f921838
David Teixeira Pereira Ferreira <davidbenavraham.ti@gmail.com>
2024-10-17 21:44:29 -0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('master')
+
+@section('content')
+    <h1>Home</h1>
+
+    <h2 class="text-3xl font-bold underline text-zinc-600">Hello world!</h2>
+
+    <button class="btn btn-success btn-sm">Button</button>
+@endsection
